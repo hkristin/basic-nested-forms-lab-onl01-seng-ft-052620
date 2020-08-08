2186f12735fd7fc347ed9394dbbf88ea39431e8b
Kristin Howard <hkristin@andrew.cmu.edu>
2020-08-08 23:56:46 +0000

    Automatically backed up by Learn

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,9 +1,3 @@
 class Ingredient < ActiveRecord::Base
   belongs_to :recipe
-
-  def recipe_attributes=(recipe)
-  self.recipe = Recipe.find_or_create_by(name: artist[:name])
-  self.recipe.update(recipe)
-  end
-
 end
